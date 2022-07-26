de6e9499e5f665103fcd2216fc1561bbc4260fdd
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-07-26 12:49:23 +0100

    web3

--- pages/web3.js
+++ pages/web3.js
@@ -1,7 +1,18 @@
 import Web3 from "web3";
 
-window.ethereum.request({ method: "eth_requestAccounts" });
+let web3;
 
-const web3 = new Web3(window.ethereum);
+//typeof window are we in: browser or server 
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+    //metamask browser
+    window.ethereum.request({ method: "eth_requestAccounts" });
+    web3 = new Web3(window.ethereum);
+} else {
+    //not running metamask
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/v3/7c26a024e9c9431db75fc7ec37bac5cd'
+    );
+    web3 = new Web3(provider);
+}
 
 export default web3;
